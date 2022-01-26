1921b8b7b018e8c5c6c8b76b77a70abe904b3db9
huangjianxiong1 <huangjianxiong1@yy.com>
2022-01-26 16:06:19 +0800

    修复SListView::EnsureVisible BUG。

--- SOUI/src/control/SListView.cpp
+++ SOUI/src/control/SListView.cpp
@@ -795,8 +795,7 @@ void SListView::EnsureVisible(int iItem)
                 OnScroll(m_bVertical, SB_BOTTOM, 0);
             else
             {
-                int pos = m_lvItemLocator->Item2Position(iItem + 1) - m_bVertical ? m_siVer.nPage
-                                                                                  : m_siHoz.nPage;
+                int pos = m_lvItemLocator->Item2Position(iItem + 1) - (m_bVertical ? m_siVer.nPage: m_siHoz.nPage);
                 OnScroll(m_bVertical, SB_THUMBPOSITION, pos);
             }
         }
@@ -817,8 +816,7 @@ void SListView::EnsureVisible(int iItem)
         }
         else
         {
-            int pos = m_lvItemLocator->Item2Position(iItem + 1) - m_bVertical ? m_siVer.nPage
-                                                                              : m_siHoz.nPage;
+            int pos = m_lvItemLocator->Item2Position(iItem + 1) - (m_bVertical ? m_siVer.nPage: m_siHoz.nPage);
             OnScroll(m_bVertical, SB_THUMBPOSITION, pos);
         }
     }
