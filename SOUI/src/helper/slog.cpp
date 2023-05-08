d6ad831e694e683f314bad6e80bde3aadf29cb20
huangjianxiong1 <huangjianxiong1@yy.com>
2023-05-08 10:44:40 +0800

    slog输出到output时增加显示线程id

--- SOUI/src/helper/slog.cpp
+++ SOUI/src/helper/slog.cpp
@@ -22,7 +22,8 @@ void Log::DefCallback(const char *tag, const char *pLogStr, int level, const cha
     {
         const int kMaxLog = Log::MAX_LOGLEN + 100;
         char *logbuf2 = (char *)malloc(kMaxLog + 1);
-        int nLen = _snprintf_s(logbuf2, kMaxLog, _TRUNCATE, "%s,%s %s %s:%d\n", tag, pLogStr, fun, file, line);
+		DWORD tid = GetCurrentThreadId();
+        int nLen = _snprintf_s(logbuf2, kMaxLog, _TRUNCATE, "pid=%u,%s,%s %s %s:%d\n",tid, tag, pLogStr, fun, file, line);
         logbuf2[nLen] = 0;
         OutputDebugStringA(logbuf2);
         free(logbuf2);
