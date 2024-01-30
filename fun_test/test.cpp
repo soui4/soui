8cce9ba335508170e55898413e48e464bd2c2100
setoutsoft <setoutsoft@qq.com>
2024-01-30 00:13:38 -0800

    update fun_test

--- fun_test/test.cpp
+++ fun_test/test.cpp
@@ -8,12 +8,11 @@ TEST(MathTest, Addition) {
 
 TEST(XmlTest,Load){
     SXmlDoc xml;
-    EXPECT_EQ(xml.load_file("/home/flyhigh/work/soui4/demo2/uires/xml/dlg_main.xml"),true);
+    EXPECT_EQ(xml.load_file("./demo2/uires/xml/dlg_main.xml"),true);
     SXmlNode node=xml.root().first_child();
     while(node){
         const wchar_t * name = node.name();
         wprintf(L"node name=%s\n",name);
         node = node.next_sibling();
     }
- EXPECT_EQ(2 + 2, 4);
 }
