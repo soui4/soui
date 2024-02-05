4cb487ef4ed0d85deb6db317d3e6454430eb5d78
setoutsoft <setoutsoft@qq.com>
2024-02-05 01:47:50 -0800

    build linux platform

---
+++ platform/linux/include/SMsgLoop.h
@@ -0,0 +1,73 @@
+﻿#ifndef __SMSGLOOP__H__
+#define __SMSGLOOP__H__
+#include <interface/smsgloop-i.h>
+#include <souicoll.h>
+#include <helper/obj-ref-impl.hpp>
+#include <helper/SCriticalSection.h>
+
+SNSBEGIN
+
+class  SMessageLoop : public TObjRefImpl<IMessageLoop> {
+  public:
+    SMessageLoop(IMessageLoop *pParentLoop);
+    virtual ~SMessageLoop();
+
+  public:
+    // Message filter operations
+    STDMETHOD_(BOOL, AddMessageFilter)(THIS_ IMsgFilter *pMessageFilter) OVERRIDE;
+
+    STDMETHOD_(BOOL, RemoveMessageFilter)(THIS_ IMsgFilter *pMessageFilter) OVERRIDE;
+
+    // Idle handler operations
+    STDMETHOD_(BOOL, AddIdleHandler)(THIS_ IIdleHandler *pIdleHandler) OVERRIDE;
+
+    STDMETHOD_(BOOL, RemoveIdleHandler)(THIS_ IIdleHandler *pIdleHandler) OVERRIDE;
+
+    // Override to change message filtering
+    STDMETHOD_(BOOL, PreTranslateMessage)(THIS_ MSG *pMsg) OVERRIDE;
+
+    // override to change idle processing
+    STDMETHOD_(BOOL, OnIdle)(THIS_ int /*nIdleCount*/) OVERRIDE;
+
+    STDMETHOD_(int, Run)(THIS) OVERRIDE;
+
+    STDMETHOD_(BOOL, IsRunning)(THIS) const OVERRIDE;
+
+    STDMETHOD_(void, OnMsg)(THIS_ LPMSG pMsg) OVERRIDE;
+
+    STDMETHOD_(void, Quit)(THIS_ int exitCode) OVERRIDE;
+
+    STDMETHOD_(BOOL, PostTask)(THIS_ IRunnable *runable) OVERRIDE;
+
+    STDMETHOD_(int, RemoveTasksForObject)(THIS_ void *pObj) OVERRIDE;
+
+    STDMETHOD_(void, ExecutePendingTask)() OVERRIDE;
+
+    STDMETHOD_(BOOL, PeekMsg)(THIS_ LPMSG pMsg, UINT wMsgFilterMin, UINT wMsgFilterMax, BOOL bRemove) OVERRIDE;
+
+    STDMETHOD_(BOOL, WaitMsg)(THIS) OVERRIDE;
+
+    STDMETHOD_(int, HandleMsg)(THIS) OVERRIDE;
+
+  public:
+    static BOOL IsIdleMessage(MSG *pMsg);
+
+  protected:
+    SArray<IMsgFilter *> m_aMsgFilter;
+    SArray<IIdleHandler *> m_aIdleHandler;
+
+    BOOL m_bRunning;
+    BOOL m_bQuit;
+    BOOL m_bDoIdle;
+    int m_nIdleCount;
+
+    SCriticalSection m_cs;
+    SList<IRunnable *> m_runnables;
+    SCriticalSection m_csRunningQueue;
+    SList<IRunnable *> m_runningQueue;
+    SAutoRefPtr<IMessageLoop> m_parentLoop;
+    DWORD m_tid;
+};
+
+SNSEND
+#endif // __SMSGLOOP__H__
---
+++ platform/linux/include/SNativeWnd.h
@@ -0,0 +1,180 @@
+﻿#ifndef __SNATIVEWND__H__
+#define __SNATIVEWND__H__
+#include <platform.h>
+#include <interface/SNativeWnd-i.h>
+#include <helper/SCriticalSection.h>
+#include <helper/obj-ref-impl.hpp>
+//////////////////////////////////////////////////////////////////////////
+// thunk 技术实现参考http://www.cppblog.com/proguru/archive/2008/08/24/59831.html
+//////////////////////////////////////////////////////////////////////////
+SNSBEGIN
+
+class SNativeWnd : public TObjRefImpl<INativeWnd> {
+  public:
+    SNativeWnd();
+    virtual ~SNativeWnd(void);
+
+    STDMETHOD_(int, GetID)(THIS) SCONST
+    {
+        return m_id;
+    }
+    STDMETHOD_(void, SetID)(THIS_ int nID)
+    {
+      m_id = nID;
+    }
+
+    STDMETHOD_(HWND, CreateNative)
+    (THIS_ LPCTSTR lpWindowName, DWORD dwStyle, DWORD dwExStyle, int x, int y, int nWidth, int nHeight, HWND hWndParent, int nID DEF_VAL(0), LPVOID lpParam DEF_VAL(0)) OVERRIDE;
+
+    STDMETHOD_(HWND, GetHwnd)(THIS) OVERRIDE;
+
+    STDMETHOD_(BOOL, SubclassWindow)(THIS_ HWND hWnd) OVERRIDE;
+
+    STDMETHOD_(HWND, UnsubclassWindow)(THIS_ BOOL bForce DEF_VAL(FALSE)) OVERRIDE;
+
+    STDMETHOD_(const MSG *, GetCurrentMessage)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(int, GetDlgCtrlID)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(DWORD, GetStyle)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(DWORD, GetExStyle)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(LONG_PTR, GetWindowLongPtr)(THIS_ int nIndex) SCONST OVERRIDE;
+
+    STDMETHOD_(LONG_PTR, SetWindowLongPtr)(THIS_ int nIndex, LONG_PTR dwNewLong) OVERRIDE;
+
+    STDMETHOD_(HWND, GetParent)(THIS) OVERRIDE;
+
+    STDMETHOD_(HWND, SetParent)(THIS_ HWND hWndNewParent) OVERRIDE;
+
+    STDMETHOD_(BOOL, IsWindowEnabled)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, ModifyStyle)
+    (THIS_ DWORD dwRemove, DWORD dwAdd, UINT nFlags DEF_VAL(0)) OVERRIDE;
+
+    STDMETHOD_(BOOL, ModifyStyleEx)
+    (THIS_ DWORD dwRemove, DWORD dwAdd, UINT nFlags DEF_VAL(0)) OVERRIDE;
+
+    STDMETHOD_(BOOL, SetWindowPos)
+    (THIS_ HWND hWndInsertAfter, int x, int y, int cx, int cy, UINT nFlags) OVERRIDE;
+
+    STDMETHOD_(BOOL, CenterWindow)(THIS_ HWND hWndCenter DEF_VAL(NULL)) OVERRIDE;
+
+    STDMETHOD_(BOOL, DestroyWindow)(THIS) OVERRIDE;
+
+    STDMETHOD_(BOOL, IsWindow)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, Invalidate)(THIS_ BOOL bErase DEF_VAL(TRUE)) OVERRIDE;
+
+    STDMETHOD_(BOOL, InvalidateRect)(THIS_ LPCRECT lpRect, BOOL bErase DEF_VAL(TRUE)) OVERRIDE;
+
+    STDMETHOD_(BOOL, GetWindowRect)(THIS_ LPRECT lpRect) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, GetClientRect)(THIS_ LPRECT lpRect) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, ClientToScreen)(THIS_ LPPOINT lpPoint) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, ClientToScreen2)(THIS_ LPRECT lpRect) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, ScreenToClient)(THIS_ LPPOINT lpPoint) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, ScreenToClient2)(THIS_ LPRECT lpRect) SCONST OVERRIDE;
+
+    STDMETHOD_(int, MapWindowPoints)
+    (THIS_ HWND hWndTo, LPPOINT lpPoint, UINT nCount) SCONST OVERRIDE;
+
+    STDMETHOD_(int, MapWindowRect)(THIS_ HWND hWndTo, LPRECT lpRect) SCONST OVERRIDE;
+
+    STDMETHOD_(UINT_PTR, SetTimer)
+    (THIS_ UINT_PTR nIDEvent, UINT nElapse, void(CALLBACK *lpfnTimer)(HWND, UINT, UINT_PTR, DWORD) DEF_VAL(NULL)) OVERRIDE;
+
+    STDMETHOD_(BOOL, KillTimer)(THIS_ UINT_PTR nIDEvent) OVERRIDE;
+
+    STDMETHOD_(HDC, GetDC)(THIS) OVERRIDE;
+
+    STDMETHOD_(HDC, GetWindowDC)(THIS) OVERRIDE;
+
+    STDMETHOD_(int, ReleaseDC)(THIS_ HDC hDC) OVERRIDE;
+
+    STDMETHOD_(BOOL, CreateCaret)(THIS_ HBITMAP hBitmap) OVERRIDE;
+
+    STDMETHOD_(BOOL, HideCaret)(THIS) OVERRIDE;
+
+    STDMETHOD_(BOOL, ShowCaret)(THIS) OVERRIDE;
+
+    STDMETHOD_(HWND, GetCapture)(THIS) OVERRIDE;
+
+    STDMETHOD_(HWND, SetCapture)(THIS) OVERRIDE;
+
+    STDMETHOD_(BOOL, ReleaseCapture)(THIS) OVERRIDE;
+
+    STDMETHOD_(HWND, SetFocus)(THIS) OVERRIDE;
+
+    STDMETHOD_(LRESULT, SendMessage)
+    (THIS_ UINT message, WPARAM wParam DEF_VAL(0), LPARAM lParam DEF_VAL(0)) OVERRIDE;
+
+    STDMETHOD_(BOOL, PostMessage)
+    (THIS_ UINT message, WPARAM wParam DEF_VAL(0), LPARAM lParam DEF_VAL(0)) OVERRIDE;
+
+    STDMETHOD_(BOOL, SendNotifyMessage)
+    (THIS_ UINT message, WPARAM wParam DEF_VAL(0), LPARAM lParam DEF_VAL(0)) OVERRIDE;
+
+    STDMETHOD_(BOOL, SetWindowText)(THIS_ LPCTSTR lpszString) OVERRIDE;
+
+    STDMETHOD_(int, GetWindowText)(THIS_ LPTSTR lpszStringBuf, int nMaxCount) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, IsIconic)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, IsZoomed)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, IsWindowVisible)(THIS) SCONST OVERRIDE;
+
+    STDMETHOD_(BOOL, MoveWindow)
+    (THIS_ int x, int y, int nWidth, int nHeight, BOOL bRepaint DEF_VAL(TRUE)) OVERRIDE;
+
+    STDMETHOD_(BOOL, MoveWindow2)(THIS_ LPCRECT lpRect, BOOL bRepaint DEF_VAL(TRUE)) OVERRIDE;
+
+    STDMETHOD_(BOOL, ShowWindow)(THIS_ int nCmdShow) OVERRIDE;
+
+    STDMETHOD_(int, SetWindowRgn)(THIS_ HRGN hRgn, BOOL bRedraw DEF_VAL(TRUE)) OVERRIDE;
+
+    STDMETHOD_(BOOL, SetLayeredWindowAttributes)
+    (THIS_ COLORREF crKey, BYTE bAlpha, DWORD dwFlags) OVERRIDE;
+
+    STDMETHOD_(BOOL, UpdateLayeredWindow)
+    (THIS_ HDC hdcDst, POINT *pptDst, SIZE *psize, HDC hdcSrc, POINT *pptSrc, COLORREF crKey, BLENDFUNCTION *pblend, DWORD dwFlags) OVERRIDE;
+
+    STDMETHOD_(void, SetMsgHandler)(THIS_ FunMsgHandler fun, void *ctx) OVERRIDE;
+    STDMETHOD_(MsgHandlerInfo *, GetMsgHandler)(THIS) OVERRIDE;
+
+    LRESULT DefWindowProc();
+    LRESULT ForwardNotifications(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled);
+    LRESULT ReflectNotifications(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL &bHandled);
+    static BOOL DefaultReflectionHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, LRESULT &lResult);
+
+  public: // EXTRACT FROM BEGIN_MSG_MAP_EX and END_MSG_MAP
+    virtual BOOL ProcessWindowMessage(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam, LRESULT &lResult, DWORD dwMsgMapID = 0);
+
+  protected:
+    LRESULT DefWindowProc(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    virtual void OnFinalMessage(HWND hWnd);
+
+    const MSG *m_pCurrentMsg;
+    BOOL m_bDestoryed;
+
+    MsgHandlerInfo m_msgHandlerInfo;
+
+  public:
+    HWND m_hWnd;
+    int  m_id;
+  protected:
+    static LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    // 只执行一次
+    static LRESULT CALLBACK StartWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+};
+
+SNSEND
+#endif // __SNATIVEWND__H__
