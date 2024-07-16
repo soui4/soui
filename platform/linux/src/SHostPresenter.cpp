4ee16cd0e190bf6a045fca6e603ad5c6d1d18314
huangjianxiong1 <huangjianxiong1@yy.com>
2024-07-16 14:28:38 +0800

    基本流程通过。

--- platform/linux/src/SHostPresenter.cpp
+++ platform/linux/src/SHostPresenter.cpp
@@ -30,7 +30,15 @@ void SHostPresenter::OnHostResize(THIS_ SIZE szHost)
 
 void SHostPresenter::OnHostPresent(THIS_ HDC hdc, IRenderTarget *pMemRT, LPCRECT rcInvalid, BYTE byAlpha)
 {
-
+    FillRect(hdc, rcInvalid, GetStockObject(WHITE_BRUSH));
+    BOOL bGetDC = hdc == 0;
+    if (bGetDC)
+        hdc = m_pNativeWnd->GetDC();
+    HDC memdc = pMemRT->GetDC(1);
+    ::BitBlt(hdc, rcInvalid->left, rcInvalid->top, rcInvalid->right-rcInvalid->left, rcInvalid->bottom-rcInvalid->top, memdc, rcInvalid->left, rcInvalid->top, SRCCOPY);
+    pMemRT->ReleaseDC(memdc, NULL);
+    if (bGetDC)
+        m_pNativeWnd->ReleaseDC(hdc);
 }
 
 void SHostPresenter::OnHostAlpha(THIS_ BYTE byAlpha)
