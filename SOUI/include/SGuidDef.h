02f530758c6aae06522b23251140381f64789452
setoutsoft <setoutsoft@qq.com>
2025-03-18 17:55:09 +0800

    impl DEFINE_SGUID2

--- SOUI/include/SGuidDef.h
+++ SOUI/include/SGuidDef.h
@@ -6,11 +6,21 @@
 #undef DEFINE_SGUID
 #endif
 
+#ifdef DEFINE_SGUID2
+#undef DEFINE_SGUID2
+#endif
+
 #ifdef INITGUID
 #define DEFINE_SGUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
         EXTERN_C const GUID SOUI_EXP name \
                 = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+#define DEFINE_SGUID2(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        EXTERN_C const GUID  name \
+                = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
 #else
 #define DEFINE_SGUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
     EXTERN_C const GUID SOUI_EXP name
+#define DEFINE_SGUID2(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        EXTERN_C const GUID  name 
 #endif // INITGUID
