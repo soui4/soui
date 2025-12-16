ab3056423ca7c1a4dcd81e1a2a35bf3b681efdb9
setoutsoft <setoutsoft@qq.com>
2025-12-16 15:00:41 +0800

    update client.

--- games/cnchess/client/SGameTheme.cpp
+++ games/cnchess/client/SGameTheme.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "SGameTheme.h"
 
 SNSBEGIN
@@ -6,135 +6,185 @@ SNSBEGIN
 
 SGameTheme::SGameTheme()
 {
+    m_pSkinPool.Attach(SUiDef::CreateSkinPool(TRUE));
+    GETUIDEF->PushSkinPool(m_pSkinPool);
 }
 
 SGameTheme::~SGameTheme()
 {
+    GETUIDEF->PopSkinPool(m_pSkinPool);
 }
 
-BOOL SGameTheme::Load(const SStringT & strPath)
+BOOL SGameTheme::Load(LPCTSTR pszThemeDir)
 {
     //set file prefix, load from file will use this prefix to build file path.
-    SApplication::getSingletonPtr()->SetFilePrefix(strPath);
+    SApplication::getSingletonPtr()->SetFilePrefix(pszThemeDir);
     SXmlDoc xmlDoc;
     if (!SApplication::getSingletonPtr()->LoadXmlDocment(xmlDoc, _T("file:config.xml")))
     {
         return FALSE;
     }
-    
     SXmlNode xmlSkin = xmlDoc.root().child(L"theme").child(L"skin");
-    int ret = SSkinPool::getSingletonPtr()->LoadSkins(&xmlSkin);
+    int ret = m_pSkinPool->LoadSkins(&xmlSkin);
 
-    //load layouts from xml file.
-    SXmlNode xmlLayouts = xmlDoc.root().child(L"theme").child(L"layouts");
-    SXmlNode xmlLayout = xmlLayouts.first_child();
-    while(xmlLayout)
+    //load widgets from xml file.
+    SXmlNode xmlWidgets = xmlDoc.root().child(L"theme").child(L"widgets");
+    SXmlNode xmlWidget = xmlWidgets.first_child();
+    while(xmlWidget)
     {
-        SStringW clsName = xmlLayout.name();
-        SStringW strName = xmlLayout.attribute(L"name").as_string();
-        SASSERT(m_mapLayouts.find(strName) == m_mapLayouts.end());
-        ILayoutParam *pLayout = SApplication::getSingletonPtr()->CreateLayoutParamByName(clsName);
-        if(pLayout)
+        SStringW clsName = xmlWidget.name();
+        SStringW strName = xmlWidget.attribute(L"name").as_string();
+        SASSERT(m_mapWidgets.find(strName) == m_mapWidgets.end());
+        IWindow *pWidget = SApplication::getSingletonPtr()->CreateWindowByName(clsName);
+        if(pWidget)
         {
-            pLayout->InitFromXml(&xmlLayout);
-            m_mapLayouts[strName] = pLayout;
-            pLayout->Release();
+            pWidget->InitFromXml(&xmlWidget);
+            m_mapWidgets[strName] = pWidget;
+            pWidget->Release();
         }
-        xmlLayout = xmlLayout.next_sibling();
+        xmlWidget = xmlWidget.next_sibling();
     }
 
-    //load animations from xml file.
-    SXmlNode xmlAnims = xmlDoc.root().child(L"theme").child(L"animations");
-    SXmlNode xmlAnim = xmlAnims.first_child();
-    while(xmlAnim)
+    //load anchors from xml file.
+    SASSERT(AnchorName::kCount == ARRAYSIZE(AnchorName::kElementNames));
+    SXmlNode xmlAnchors = xmlDoc.root().child(L"theme").child(L"anchors");
+    for(int i=0;i<ARRAYSIZE(AnchorName::kElementNames);i++)
     {
-        SStringW clsName = xmlAnim.name();
-        SStringW strName = xmlAnim.attribute(L"name").as_string();
-        SASSERT(m_mapAnims.find(strName) == m_mapAnims.end());
-        IAnimation *pAnim = SApplication::getSingletonPtr()->CreateAnimationByName(clsName);
-        if(pAnim)
-        {
-            pAnim->InitFromXml(&xmlAnim);
-            m_mapAnims[strName] = pAnim;
-            pAnim->Release();
+        SStringW strName = AnchorName::kElementNames[i];
+        AnchorPos pos;
+        SXmlNode xmlEle = xmlAnchors.child(strName);
+        if(xmlEle){
+            SAnchorLayoutParam::ParsePosition(xmlEle.attribute(L"pos").value(), pos);
+            pos.fOffsetX = xmlEle.attribute(L"offsetX").as_float();
+            pos.fOffsetY = xmlEle.attribute(L"offsetY").as_float();
         }
-        xmlAnim = xmlAnim.next_sibling();
+        m_mapLayout[strName] = pos;
     }
 
-    //load sprites from xml file.
-    SXmlNode xmlSprites = xmlDoc.root().child(L"theme").child(L"sprites");
-    SXmlNode xmlSprite = xmlSprites.first_child();
-    while(xmlSprite)
+    //load animation
+    SXmlNode xmlAnim = xmlDoc.root().child(L"theme").child(L"anims");
+    SXmlNode xmlAni = xmlAnim.first_child();
+    while(xmlAni)
     {
-        SStringW clsName = xmlSprite.name();
-        SStringW strName = xmlSprite.attribute(L"name").as_string();
-        SASSERT(m_mapSprites.find(strName) == m_mapSprites.end());
-        ISprite *pSprite = SApplication::getSingletonPtr()->CreateSpriteByName(clsName);
-        if(pSprite)
-        {
-            pSprite->InitFromXml(&xmlSprite);
-            m_mapSprites[strName] = pSprite;
-            pSprite->Release();
+        SStringW strName = xmlAni.attribute(L"name").as_string();
+        SStringT strSrc = S_CW2T(xmlAni.attribute(L"src").as_string());
+        SASSERT(m_mapAnimations.find(strName) == m_mapAnimations.end());
+        SXmlDoc xml;
+        if (SApplication::getSingletonPtr()->LoadXmlDocment(xml, strSrc)){
+            SAutoRefPtr<IAnimation> pAni(SApplication::getSingletonPtr()->CreateAnimationByName(xml.root().first_child().name()),false);
+            if (pAni){
+                SXmlNode xmlNode = xml.root().first_child();
+                pAni->InitFromXml(&xmlNode);
+                m_mapAnimations[strName] = pAni;
+            }
         }
-        xmlSprite = xmlSprite.next_sibling();
+        xmlAni = xmlAni.next_sibling();
     }
 
-    //load sounds from xml file.
+    //copy template
+    SXmlNode xmlTemplate = xmlDoc.root().child(L"theme").child(L"template");
+    SASSERT(xmlTemplate);
+    m_docTemplate.root().append_copy(xmlTemplate);
+
     SXmlNode xmlSounds = xmlDoc.root().child(L"theme").child(L"sounds");
-    SXmlNode xmlSound = xmlSounds.first_child();
-    while(xmlSound)
+    SASSERT(xmlSounds);
+    m_docSounds.root().append_copy(xmlSounds);
+
+    return ret > 0 ;
+}
+
+IWindow *SGameTheme::GetWidget(LPCWSTR pszName)
+{
+    std::map<SStringW, SAutoRefPtr<IWindow> >::iterator it = m_mapWidgets.find(pszName);
+    if(it != m_mapWidgets.end())
     {
-        SStringW clsName = xmlSound.name();
-        SStringW strName = xmlSound.attribute(L"name").as_string();
-        SASSERT(m_mapSounds.find(strName) == m_mapSounds.end());
-        IAudioPlayer *pSound = SApplication::getSingletonPtr()->CreateAudioPlayerByName(clsName);
-        if(pSound)
+        return it->second;
+    }   
+    return nullptr;
+}
+
+SXmlNode SGameTheme::GetTemplate(LPCWSTR pszName)
+{
+    return m_docTemplate.root().first_child().child(pszName);
+}
+
+IAnimation *SGameTheme::GetAnimation(LPCWSTR pszName)
+{
+    std::map<SStringW, SAutoRefPtr<IAnimation> >::iterator it = m_mapAnimations.find(pszName);
+    if(it != m_mapAnimations.end())
+    {
+        return it->second;
+    }   
+    return nullptr;
+}
+
+SIZE SGameTheme::GetCardSize() const
+{
+    ISkinObj *pCard = GETSKIN(L"skin_card",100);
+    return pCard->GetSkinSize();
+}
+
+SStringW SGameTheme::GetEffectSoundFile(LPCWSTR pszName)
+{
+    SXmlNode xmlEffect = m_docSounds.root().first_child().child(L"effects").first_child();
+    while (xmlEffect)
+    {
+        SStringW name = xmlEffect.attribute(L"name").as_string();
+        if (name == pszName)
         {
-            pSound->InitFromXml(&xmlSound);
-            m_mapSounds[strName] = pSound;
-            pSound->Release();
+            // 找到匹配的效果音效，获取src属性
+            SStringW src = xmlEffect.attribute(L"src").as_string();
+            if (!src.IsEmpty())
+            {
+                return src;
+            }
+            break;
         }
-        xmlSound = xmlSound.next_sibling();
+        xmlEffect = xmlEffect.next_sibling();
     }
-
-    return TRUE;
+    return L"";
 }
 
-ISkinObj * SGameTheme::GetSkin(const SStringW & name)
+SStringW SGameTheme::GetProcSoundFile(BOOL bMale,LPCWSTR pszName)
 {
-    return SSkinPool::getSingletonPtr()->GetSkin(name);
-}
+    SXmlNode xmlSounds = m_docSounds.root().first_child().child(L"processes");
+    // 根据性别选择对应的节点(male或female)
+    SXmlNode xmlProcess = xmlSounds.child(bMale ? L"male" : L"female").first_child();
+    while (xmlProcess)
+    {
+        SStringW name = xmlProcess.attribute(L"name").as_string();
+        if (name == pszName)
+        {
+            float weight = 0.0f;
+            SXmlNode xmlFile = xmlProcess.first_child();
+            while (xmlFile)
+            {
+                weight += xmlFile.attribute(L"weight").as_float(1.0f);
+                xmlFile = xmlFile.next_sibling();
+            }
 
-IAnimation * SGameTheme::GetAnimation(const SStringW & name)
-{
-    ANIMATION_MAP::const_iterator it = m_mapAnims.find(name);
-    if(it!=m_mapAnims.end())
-        return it->second;
-    return NULL;
-}
-
-ILayoutParam * SGameTheme::GetLayout(const SStringW & name)
-{
-    LAYOUT_MAP::const_iterator it = m_mapLayouts.find(name);
-    if(it!=m_mapLayouts.end())
-        return it->second;
-    return NULL;
-}
-
-ISprite * SGameTheme::GetSprite(const SStringW & name)
-{
-    SPRITE_MAP::const_iterator it = m_mapSprites.find(name);
-    if(it!=m_mapSprites.end())
-        return it->second;
-    return NULL;
-}
-
-IAudioPlayer * SGameTheme::GetSound(const SStringW & name)
-{
-    SOUND_MAP::const_iterator it = m_mapSounds.find(name);
-    if(it!=m_mapSounds.end())
-        return it->second;
-    return NULL;
+            int nWeight= (int)(weight * 100);
+            if (nWeight == 0){
+                break;
+            }
+            int nRand = rand() % nWeight;
+            xmlFile = xmlProcess.first_child();
+            while (xmlFile)
+            {
+                nRand -= (int)(xmlFile.attribute(L"weight").as_float(1.0f) * 100);
+                if (nRand < 0){
+                    break;
+                }
+                xmlFile = xmlFile.next_sibling();
+            }
+            if (!xmlFile){
+                break;
+            }
+            return xmlFile.attribute(L"src").as_string();
+        }
+        xmlProcess = xmlProcess.next_sibling();
+    }
+    return L"";
 }
 
 SNSEND
--- games/cnchess/client/stdafx.h
+++ games/cnchess/client/stdafx.h
@@ -1,53 +1,42 @@
-#pragma once
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
 
-#include <souistd.h>
-#include <core/SHostWnd.h>
-#include <control/SMessageBox.h>
-#include <helper/SMenuEx.h>
-#include <helper/SMenu.h>
-#include <helper/STrayIcon.h>
-#include <helper/SHostDialog.h>
-#include <helper/findplace.h>
-#include <helper/SkinPool.h>
-#include <helper/templethelper.hpp>
-#include <layout/SLinearLayout.h>
-#include <layout/SHBox.h>
-#include <layout/SVBox.h>
-#include <control/SButton.h>
-#include <control/SCheckBox.h>
-#include <control/SRadioBox.h>
-#include <control/SSliderBar.h>
-#include <control/STabCtrl.h>
-#include <control/SScrollBar.h>
-#include <control/SHeaderCtrl.h>
-#include <control/SListCtrl.h>
-#include <control/SListBox.h>
-#include <control/SComboBox.h>
-#include <control/STreeCtrl.h>
-#include <control/SMCListView.h>
-#include <control/SRichEdit.h>
-#include <control/SCalendar.h>
-#include <control/SSpinButtonCtrl.h>
-#include <control/SActiveX.h>
-#include <control/SSplitPane.h>
-#include <control/SFlashCtrl.h>
-#include <control/SIECtrl.h>
-#include <control/SMediaPlayer.h>
-#include <control/SChart.h>
-#include <control/SWordBreak.h>
-#include <gif/SAniImgFrame.h>
-#include <gif/SGifPlayer.h>
-#include <activex/flash11.tlh>
-#include <animator/SInterpolatorImpl.h>
-#include <SApp.h>
-#include <interface/ws-i.h>
-#include <helper/obj-ref-impl.hpp>
-#include <com-loader.hpp>
+#if !defined(AFX_STDAFX_H__A9DB83DB_A9FD_11D0_BFD1_444553540000__INCLUDED_)
+#define AFX_STDAFX_H__A9DB83DB_A9FD_11D0_BFD1_444553540000__INCLUDED_
 
-#ifdef _DEBUG
-    #define SYS_NAMED_RESOURCE_LUA  _T("soui_named_resource.lua")
-#else
-    #define SYS_NAMED_RESOURCE_LUA  _T("soui_named_resource.lua")
+// Modify the following defines if you have to target a platform prior to the ones specified below.
+// Refer to MSDN for the latest info on corresponding values for different platforms.
+#ifndef WINVER              // Allow use of features specific to Windows XP or later.
+#define WINVER 0x0501       // Change this to the appropriate value to target other versions of Windows.
 #endif
 
+#ifndef _WIN32_WINNT        // Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501 // Change this to the appropriate value to target other versions of Windows.
+#endif                      
+
+#ifndef _WIN32_WINDOWS      // Allow use of features specific to Windows 98 or later.
+#define _WIN32_WINDOWS 0x0410 // Change this to the appropriate value to target Windows Me or later.
+#endif
+
+#ifndef _WIN32_IE           // Allow use of features specific to IE 6.0 or later.
+#define _WIN32_IE 0x0600    // Change this to the appropriate value to target other versions of IE.
+#endif
+
+
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+
+#include <windows.h>
+#include <tchar.h>
+
+// TODO: reference additional headers your program requires here
+#include <souistd.h>
+#include <core/SHostWnd.h>
+#include <core/SWnd.h>
+#include <core/SHostDialog.h>
+#include <helper/SMenuEx.h>
+#include <gif/SSkinAni.h>
+
 using namespace SOUI;
+#endif // !defined(AFX_STDAFX_H__A9DB83DB_A9FD_11D0_BFD1_444553540000__INCLUDED_)
