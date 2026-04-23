1097c159a1a58c2a42481f17dc98b6e3486cf595
setoutsoft <setoutsoft@qq.com>
2026-04-23 12:04:52 +0800

    fix file utf8

--- controls.extend/SLrcView.cpp
+++ controls.extend/SLrcView.cpp
@@ -1,4 +1,4 @@
-#include "stdafx.h"
+﻿#include "stdafx.h"
 #include "SLrcView.h"
 #include <algorithm>
 #include <string>
--- controls.extend/SLrcView.h
+++ controls.extend/SLrcView.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <list>
 #include <vector>
 SNSBEGIN
