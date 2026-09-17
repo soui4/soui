8c0dd465291656c009f1fc34f32b5be9d8e096bc
setoutsoft <setoutsoft@qq.com>
2026-09-17 16:08:28 +0800

    fix config path.

--- games/cnchess/server/server.cpp
+++ games/cnchess/server/server.cpp
@@ -142,7 +142,7 @@ int run(LPCTSTR pszCfg){
 
 int _tmain(int argc, TCHAR **argv)
 {
-    LPCTSTR pszCfg = _T("./config.xml");
+    LPCTSTR pszCfg = _T("./config/config.xml");
     if(argc > 1)
         pszCfg = argv[1];
     return run(pszCfg);
